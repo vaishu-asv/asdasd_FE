b9deb4befe347990ccd9430362a193317577e67f
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 13:12:41 +0530

    Initial commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,11 +1,8 @@
 import { ScheduleComponent } from './schedule/schedule.component';
-import { SavedSchedulecommonComponent } from './saved-schedulecommon/saved-schedulecommon.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'saved-schedulecommon', component: SavedSchedulecommonComponent },
-
   { path: 'schedule', component: ScheduleComponent },
 ];
 
